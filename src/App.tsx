0e4ee403c7656d2d4e17129a7f6a3d4fbb327970
nathandao <nathan.dao@futurice.com>
2018-10-18 10:07:00 +0300

    update content and stylings

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,15 @@ class App extends React.Component {
         </header>
         <p>> welcome to futulabs 3d printer portal, u shall proceed...</p>
         <p>
-          > <a href={`${baseUrl}/trevor/`}>trevor</a>: TAZ6-DualExtruder(v2) ,
-          <a href={`${baseUrl}/pepe/`}>pepe</a>: TAZ6-SingleExtruder
+          > <a href={`${baseUrl}/trevor/`}>TREVOR</a>: TAZ6-DualExtruder(v2) ,
+          <a href={`${baseUrl}/pepe/`}>PEPE</a>: TAZ6-SingleExtruder
+        </p>
+        <p>
+          > if you're new to the TAZ printers,{" "}
+          <a href="https://www.lulzbot.com/learn/tutorials/your-second-print">
+            this tutorial
+          </a>{" "}
+          should get you started
         </p>
         <div className="camera-container">
           <a
@@ -26,6 +33,22 @@ class App extends React.Component {
             p3p3
           </a>
         </div>
+        <p>
+          <b>
+            ADDITIONAL INSTRUCTION FOR Trevor (which has a DualExtruder V2):
+          </b>
+        </p>
+        <p>
+          First switch Cura Printer to a TAZ 6 with DualExtruder. If the printer
+          is not yet created in Cura, create a new one in{" "}
+          <b>
+            Settings > Printer > Add Printer > Choose options TAZ 6,
+            DualExtruder v2 > Add Printer > In the Hot End 2 tab, set Nozzle
+            offset X to 0.695 and Nozzle offset Y to -50.225
+          </b>{" "}
+          (these are the calibrated values for our DualNozzle and varries
+          between Nozzle units)
+        </p>
       </div>
     );
   }
